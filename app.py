19ecf96c12c8a02c8c18eb98f246d26ac3305cec
Akib <akib@Akibs-MacBook-Air.local>
2023-07-11 01:15:28 +0530

    added code for getting all articles and some articles

---
+++ app.py
@@ -0,0 +1,26 @@
+from chalice import Chalice
+from chalicelib.articles import article_list
+
+app = Chalice(app_name='akibblogs-backend')
+
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+
+@app.route('/articles')
+def articles():
+    return article_list
+
+
+@app.route('/articles/{article_id}')
+def get_one_article(article_id):
+    for article in article_list:
+        print(type(article.get('id')))
+        print(type(article_id))
+
+        if article.get('id') == article_id:
+            return article
+
+    raise Exception("Something went wrong")
